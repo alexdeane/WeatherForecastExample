d6a03f7a04ae335168dea1ba38dcf456b639d67c
Alex Deane <adeane999@gmail.com>
2023-02-15 15:26:45 -0500

    I guess it just works? lol

--- WeatherForecastExample.Web/ClientApp/src/app/services/api-service.ts
+++ WeatherForecastExample.Web/ClientApp/src/app/services/api-service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {ServiceResult, WeatherForecastResult} from "../models/weather-forecast";
-import { environment } from '../../environments/environment'
 import {Observable} from "rxjs";
 
 @Injectable()
@@ -10,7 +9,7 @@ export class ApiService {
   }
 
   public searchForecasts(search: string) : Observable<HttpResponse<ServiceResult<WeatherForecastResult>>>{
-    return this.http.get<ServiceResult<WeatherForecastResult>>(`${environment.apiBaseUrl}api/weatherForecast?search=${search}`, {
+    return this.http.get<ServiceResult<WeatherForecastResult>>(`api/weatherForecast?search=${search}`, {
       observe: "response"
     });
   }
--- WeatherForecastExample.Web/ClientApp/src/environments/environment.ts
+++ WeatherForecastExample.Web/ClientApp/src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:7050/'
+  production: false
 };
 
 /*
